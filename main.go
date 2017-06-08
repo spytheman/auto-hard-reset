100d27aaf63a1fd831f431c7eca1b42c65663386
Delyan Angelov <delian66@gmail.com>
2017-06-08 20:10:15 +0300

    Add configuration file in JSON format: config.json .

--- main.go
+++ main.go
@@ -5,34 +5,50 @@ import (
 
 	logging "github.com/op/go-logging"
 
+	"encoding/json"
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
+	"io/ioutil"
+	"os"
 )
 
-var log = logging.MustGetLogger("aut-hard-reset-log")
+type MinerConfig struct {
+	Name string // NAME
+	Pin  string // PIN-NUMBER OF GPIO
+	Ip   string // IP ADDRESS
+	Info string // ADDITIONAL INFO
+}
+
+var log = logging.MustGetLogger("auto-hard-reset-log")
 
 func main() {
+	log.Notice("Reading file config.json...")
+	configFileContent, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		log.Error("Trying to read file config.json, but:", err)
+		os.Exit(1)
+	}
+
+	log.Notice("Parsing configuration file...")
+	var minerConfigs []MinerConfig
+	err = json.Unmarshal(configFileContent, &minerConfigs)
+	if err != nil {
+		log.Error("Parsing JSON content, but:", err)
+		os.Exit(2)
+	}
+
+	totalMinerConfigs := len(minerConfigs)
+	log.Notice("Found miner configurations:", totalMinerConfigs)
+	
 	r := raspi.NewAdaptor()
-
 	///MINING RIGS CONFIGURATION///
-	var miningRigs [12]Rig //number of machines
-
-	//EXAMPLE
-	//miningRigs[num] = &Rig{"NAME", gpio.NewRelayDriver(r, "PIN-NUMBER OF GPIO"), "IP ADDRESS", "ADDITIONAL INFO"}
-	miningRigs[0] = Rig{"machine 1", gpio.NewRelayDriver(r, "40"), "192.168.0.100", "R9 290's"}
-	miningRigs[1] = Rig{"machine 2", gpio.NewRelayDriver(r, "38"), "192.168.0.101", "RX480's"}
-	miningRigs[2] = Rig{"machine 3", gpio.NewRelayDriver(r, "37"), "192.168.0.102", "RX480's"}
-	miningRigs[3] = Rig{"machine 4", gpio.NewRelayDriver(r, "36"), "192.168.0.103", "RX480's"}
-	miningRigs[4] = Rig{"machine 5", gpio.NewRelayDriver(r, "35"), "192.168.0.104", "RX480's"}
-	miningRigs[5] = Rig{"machine 6", gpio.NewRelayDriver(r, "33"), "192.168.0.105", "RX480's"}
-	miningRigs[6] = Rig{"machine 7", gpio.NewRelayDriver(r, "32"), "192.168.0.106", "RX480's"}
-	miningRigs[7] = Rig{"machine 8", gpio.NewRelayDriver(r, "31"), "192.168.0.107", "RX480's"}
-	miningRigs[8] = Rig{"machine 9", gpio.NewRelayDriver(r, "29"), "192.168.0.108", "RX480's"}
-	miningRigs[9] = Rig{"machine 10", gpio.NewRelayDriver(r, "22"), "192.168.0.109", "RX480's"}
-	miningRigs[10] = Rig{"machine 11", gpio.NewRelayDriver(r, "18"), "192.168.0.110", "RX480's"}
-	miningRigs[11] = Rig{"machine 12", gpio.NewRelayDriver(r, "16"), "192.168.0.111", "RX480's"}
-	///END OF MINING RIG CONFIGURATION///
+	miningRigs := make([]Rig, 0)
+	for _, m := range minerConfigs {
+		log.Notice("minerConfig:", m)
+		miningRigs = append(miningRigs, Rig{m.Name, gpio.NewRelayDriver(r, m.Pin), m.Ip, m.Info})
+	}
+	log.Notice("Configured rigs: ", len(miningRigs))
 
 	LogMachines()
 
